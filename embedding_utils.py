b543293056a64f4b311c98db87c7c3d24627b79e
Rustam Shiriyev <118140621+Rustamshry34@users.noreply.github.com>
2025-10-29 18:56:40 +0100

    Update embedding_utils.py

--- embedding_utils.py
+++ embedding_utils.py
@@ -3,7 +3,7 @@ os.environ["SENTENCE_TRANSFORMERS_DISABLE_ONNX"] = "1"
 
 
 from sentence_transformers import SentenceTransformer
-model = SentenceTransformer("sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2")
+model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
 
 
 def generate_embedding(text):
@@ -11,5 +11,6 @@ def generate_embedding(text):
     if not text or not isinstance(text, str) or text.strip() == "":
         raise ValueError("Input text is empty or invalid.")
     return model.encode(text)
+
 
     
